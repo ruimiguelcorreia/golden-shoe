e37825b8e7a17d5dc01ed7f66fd54633e75e6940
Rui Correia <rmgc31@gmail.com>
2020-01-14 00:13:52 +0000

    logo testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import logo from './img/golden-shoe-logo.png';
 
 function App() {
   return (
     <div className="App">
       <h1>Golden Shoe</h1>
+      <img src={logo} alt='' />
     </div>
   );
 }
